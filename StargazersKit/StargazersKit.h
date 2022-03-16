3035e5772f41e835dc069e5c0324bc47f6d80021
Roberto Casula <roberto.casula@injenia.it>
2022-03-16 18:22:47 +0100

    Initial Commit

---
+++ StargazersKit/StargazersKit.h
@@ -0,0 +1,18 @@
+//
+//  StargazersKit.h
+//  StargazersKit
+//
+//  Created by Roberto Casula on 16/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StargazersKit.
+FOUNDATION_EXPORT double StargazersKitVersionNumber;
+
+//! Project version string for StargazersKit.
+FOUNDATION_EXPORT const unsigned char StargazersKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StargazersKit/PublicHeader.h>
+
+
