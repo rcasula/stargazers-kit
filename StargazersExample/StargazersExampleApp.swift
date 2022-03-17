b6d983d200f31cb36c9035386bcd2927b5e0b25b
Roberto Casula <roberto.casula@injenia.it>
2022-03-17 13:11:02 +0100

    Added sample project

---
+++ StargazersExample/StargazersExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StargazersExampleApp.swift
+//  StargazersExample
+//
+//  Created by Roberto Casula on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct StargazersExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
